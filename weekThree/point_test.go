607eb7d0fd3a990ca78eb8b15ce581fd4e87c86c
Andrew Plunk <aplunk@cloudflare.com>
2017-10-09 14:31:12 -0500

    Add point test

---
+++ weekThree/point_test.go
@@ -0,0 +1,58 @@
+package weekThree
+
+import (
+	"math"
+	"testing"
+)
+
+type test func(*testing.T, points)
+type expectation func(int, int, bool) test
+
+func expectSlope(i, j int, expected float64) test {
+	return func(t *testing.T, p points) {
+		actual := p.slope(i, j)
+		if actual != expected {
+			t.Fatalf("Expected %f != Actual %f", expected, actual)
+		}
+	}
+}
+
+func expectCompare(i, j int, expected bool) test {
+	return func(t *testing.T, p points) {
+		actual := p.compare(i, j)
+		if actual != expected {
+			t.Fatalf("Expected %t != Actual %t", expected, actual)
+		}
+	}
+}
+
+func TestPoints(t *testing.T) {
+	points := points{
+		point{0, 0},
+		point{-1, 0},
+		point{10, 10},
+		point{10, 12},
+	}
+	tests := []test{
+		// x break ties false
+		expectCompare(0, 1, false),
+		// x break ties true
+		expectCompare(1, 0, true),
+		// i.y < j.y
+		expectCompare(1, 2, true),
+		// j.y < i.y
+		expectCompare(2, 1, false),
+		// degenerate case(equal points) == negative infinity
+		expectSlope(1, 1, math.Inf(-2)),
+		// vertical line segment positive infinity
+		expectSlope(2, 3, math.Inf(1)),
+		// horizontal line segment
+		expectSlope(0, 1, 0),
+		// normal slope
+		expectSlope(0, 2, float64(1)),
+	}
+
+	for _, test := range tests {
+		test(t, points)
+	}
+}
